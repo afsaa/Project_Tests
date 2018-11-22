95f37dc2b1d7a533495322d536c39f41713759ff
unknown <afsaa@utp.edu.co>
2018-11-21 20:58:02 -0500

    Colas persistentes y temporales listas

--- Test_Rabbit/rpc_client.js
+++ Test_Rabbit/rpc_client.js
@@ -1,32 +1,34 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require("amqplib/callback_api");
 
-var cola_server = 'conectoMesa';
-var enviar_votos='atencionMesas';
+// Colas Persistentes
+var q_conexion = "conectoMesa";
+var q_votos = "atencionMesas";
 
 // Colas Temporales
-var q_name = '';
-var q_canciones='canciones';
+var q_canciones = "canciones";
 
 function bail(err) {
   console.error(err);
   process.exit(1);
 }
 
-function consumer(conn) {
+function persistentQueueOperations(conn, receiverQueue) {
   var ok = conn.createChannel(on_open);
   function on_open(err, ch) {
     if (err != null) bail(err);
-    ch.assertQueue('', { exclusive: true }, function (err, q) {
-      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-
-    // ch.assertQueue('', { exclusive: true }, function (err, q2) {
-    //   console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q2.queue);
-
-      ch.sendToQueue(cola_server, new Buffer('AsRt67'+','+q_canciones), {
+    // Creamos la cola persistente "q" la cual contendrá la respuesta para el sitio.
+    ch.assertQueue("", { durable: true }, function(err, q) {
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        q.queue
+      );
+      // Enviamos la información a la cola indicada y pedimos que se nos responda a "q.queue".
+      ch.sendToQueue(receiverQueue, new Buffer("AsRt67" + "," + q_canciones), {
+        persistent: true,
         replyTo: q.queue
       });
-
-      ch.consume(q.queue, function (msg) {
+      // Consumimos los datos recibidos para administrarlos.
+      ch.consume(q.queue, function(msg) {
         if (msg !== null) {
           msg = msg.content.toString();
           //var obj = JSON.parse(msg);
@@ -38,11 +40,41 @@ function consumer(conn) {
   }
 }
 
-amqp.connect('amqp://www.nidal.online', function(err, conn) {
+function temporalQueueOperations(conn, receiverQueue) {
+  var ok = conn.createChannel(on_open);
+  function on_open(err, ch) {
     if (err != null) bail(err);
-    // Creamos el canal y enviamos el código del sitio.
-    //conn.createChannel(function(err, ch) {
-    
-    // Recibimos la respuesta del servidor
-    consumer(conn);
-});
+    // Creamos la cola temporal "q" la cual contendrá la respuesta para el sitio.
+    ch.assertQueue("", { exclusive: true }, function(err, q) {
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        q.queue
+      );
+      // Enviamos la información a la cola indicada y pedimos que se nos responda a "q.queue".
+      ch.sendToQueue(receiverQueue, new Buffer("AsRt67" + "," + q_canciones), {
+        replyTo: q.queue
+      });
+      // Consumimos los datos recibidos para administrarlos.
+      ch.consume(q.queue, function(msg) {
+        if (msg !== null) {
+          msg = msg.content.toString();
+          //var obj = JSON.parse(msg);
+          console.log(msg);
+          //console.log(obj);
+        }
+      });
+    });
+  }
+}
+
+amqp.connect(
+  "amqp://www.nidal.online",
+  function(err, conn) {
+    if (err != null) bail(err);
+    // Recibimos la respuesta del servidor por medio de las colas persistentes.
+    persistentQueueOperations(conn, q_conexion);
+    persistentQueueOperations(conn, q_votos);
+    // Recibimos la respuesta del servidor por medio de las colas temporales.
+    temporalQueueOperations(conn, q_canciones);
+  }
+);
